8e1e89c7c461902858f86450c166245071d3097e
Dario Motto <dario_aratto@hotmail.com>
2016-12-01 12:25:21 -0600

    Detalles Subasta Mejorado

--- application/views/front_end/subasta_detalle.php
+++ application/views/front_end/subasta_detalle.php
@@ -8,84 +8,161 @@
             <!-- Section's title goes here -->
             <p>Detalle de Subasta.</p>
             <!--Simple description for section goes here. -->
+
         </div>
         <?php if ($Inscritos) { ?>
             <?php foreach ($Inscritos as $i) { ?>
                 <?php if ($this->session->userdata('id') == $i->UsuarioInscrito | $this->session->userdata('id') == $SubastaDetalle->Propietario) { ?>
                     <?php if ($Inscritos == TRUE & $this->session->userdata('id') != $SubastaDetalle->Propietario) { ?>
-        <?php foreach ($Inscritos as $i) { ?>
-            <?php if ($this->session->userdata('id') == $i->UsuarioInscrito) { ?>
-                <!--INSCRITO -->
-                <div>
-                    <div class="pull-right col-xs-5">
-                        <?php if ($SubastaDetalle->foto) { ?>
-                            <img class="img-rounded" src="<?= base_url(); ?>public/subasta_ci/producto/<?= $SubastaDetalle->foto; ?>" alt="project 3" >
-                        <?php } else { ?>
-                            <img class="img-rounded" src="<?= base_url(); ?>public/images/Portfolio01.png" alt="project 3" >
-                        <?php } ?>
-                    </div>
-                    <div class=" col-xs-5">
-                        <div class="project-title"></div>
-                        <div class="project-info">
-                            <h1><?= $SubastaDetalle->nombreProducto; ?></h1>
-                            <hr>
-                            <div class="row-margin">
-                                <span>Monto Inicial</span> 
-                                <span><h2><strong>$<?= $SubastaDetalle->precioSujerido; ?></strong></h2></span>
-                            </div>
-                        </div>
-                        <form class="form-horizontal">
-                            <div class="col-xs-12 form-group">
-                                <div class="row-margin">
-                                    <span><h2><strong><input type="number" class="form-control" id="exampleInputAmount" placeholder="Ofertar"></strong></h2></span>
+                        <?php foreach ($Inscritos as $i) { ?>
+                            <?php if ($this->session->userdata('id') == $i->UsuarioInscrito) { ?>
+                                <!--INSCRITO -->
+                                <div>
+                                    <div class="pull-right col-xs-5">
+                                        <?php if ($SubastaDetalle->foto) { ?>
+                                            <img class="img-rounded" src="<?= base_url(); ?>public/subasta_ci/producto/<?= $SubastaDetalle->foto; ?>" alt="project 3" >
+                                        <?php } else { ?>
+                                            <img class="img-rounded" src="<?= base_url(); ?>public/images/Portfolio01.png" alt="project 3" >
+                                        <?php } ?>
+                                    </div>
+                                    <div class=" col-xs-5">
+                                        <div class="project-title"></div>
+                                        <div class="project-info">
+                                            <h1><?= $SubastaDetalle->nombreProducto; ?></h1>
+                                            <hr>
+                                            <div class="row-margin">
+                                                <span>Monto Inicial</span> 
+                                                <span><h2><strong>$<?= $SubastaDetalle->precioSujerido; ?></strong></h2></span>
+                                            </div>
+                                        </div>
+                                        <form class="form-horizontal">
+                                            <div class="col-xs-12 form-group">
+                                                <div class="row-margin">
+                                                    <span><h2><strong><input type="number" class="form-control" id="exampleInputAmount" placeholder="Ofertar"></strong></h2></span>
+                                                </div>
+                                                <div class="input-group col-xs-12">
+                                                    <div class="input-group-addon">
+                                                        <button type="submit" class="btn btn-primary btn-large">Ofertar</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                        <div class="embed-responsive embed-responsive-16by9">
+                                            <iframe class="embed-responsive-item" src="<?= base_url(); ?>oferta/index/<?= $SubastaDetalle->id; ?>"></iframe>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="input-group col-xs-12">
-                                    <div class="input-group-addon">
-                                        <button type="submit" class="btn btn-primary btn-large">Ofertar</button>
+                                <div>
+                                    <div class=" col-xs-10">
+                                        <div class="project-description">
+                                            <div class="project-title clearfix">
+                                                <h3><strong><?= $SubastaDetalle->nombreProducto; ?></strong></h3>
+                                            </div>
+                                            <div class="project-info">
+                                                <div>
+                                                    <span>Fecha</span><strong><?= $SubastaDetalle->fechaSubasta; ?></strong></div>
+                                                <div>
+                                                    <span>Lote</span>
+                                                    <?php if ($SubastaDetalle->IsLote == 1) { ?>
+                                                        <strong>SI</strong> </div>
+                                                <?php } else { ?>
+                                                    <strong>NO</strong> </div>
+                                            <?php } ?>
+                                            <div>
+                                                <span>Producto</span><strong><?= $SubastaDetalle->TipoProductoNombre; ?></strong></div>
+                                            <div>
+                                                <span>Inscripcion</span><br><strong><?= $SubastaDetalle->fechaInscripcion; ?></strong></div>
+                                        </div>
+                                        <div class="col-xs-10">
+                                            <p><?= $SubastaDetalle->descripcion; ?></p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </form>
-                        <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="<?= base_url(); ?>oferta/index/<?= $SubastaDetalle->id; ?>"></iframe>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div class=" col-xs-10">
-                        <div class="project-description">
-                            <div class="project-title clearfix">
-                                <h3><strong><?= $SubastaDetalle->nombreProducto; ?></strong></h3>
-                            </div>
-                            <div class="project-info">
-                                <div>
-                                    <span>Fecha</span><strong><?= $SubastaDetalle->fechaSubasta; ?></strong></div>
-                                <div>
-                                    <span>Lote</span>
-                                    <?php if ($SubastaDetalle->IsLote == 1) { ?>
-                                        <strong>SI</strong> </div>
+                            <!--FIN INSCRITO -->
+                        <?php } ?>
+                    <?php } ?>
+                    <?php
+                } else {
+                    if ($this->session->userdata('id') == $SubastaDetalle->Propietario) {
+                        ?>
+                        <!--PROPIETARIO -->
+                        <div>
+                            <div class="pull-right col-xs-5">
+                                <?php if ($SubastaDetalle->foto) { ?>
+                                    <img class="img-thumbnail" src="<?= base_url(); ?>public/subasta_ci/producto/<?= $SubastaDetalle->foto; ?>" alt="project 3" >
                                 <?php } else { ?>
-                                    <strong>NO</strong> </div>
-                            <?php } ?>
-                            <div>
-                                <span>Producto</span><strong><?= $SubastaDetalle->TipoProductoNombre; ?></strong></div>
-                            <div>
-                                <span>Inscripcion</span><br><strong><?= $SubastaDetalle->fechaInscripcion; ?></strong></div>
+                                    <img class="img-thumbnail" src="<?= base_url(); ?>public/images/Portfolio01.png" alt="project 3" >
+                                <?php } ?>
+                            </div>
+                            <div class=" col-xs-5">
+                                <div class="project-title"></div>
+                                <div class="project-info">
+                                    <h1><?= $SubastaDetalle->nombreProducto; ?></h1>
+                                    <hr>
+                                    <div class="row-margin">
+                                        <span>Monto Inicial</span> 
+                                        <span><h2><strong>$<?= $SubastaDetalle->precioSujerido; ?></strong></h2></span>
+                                    </div>
+                                </div>
+                                <form class="form-horizontal">
+                                    <div class="input-group col-xs-12">
+                                        <div class="input-group-addon">
+                                            <button type="submit" class="btn btn-primary btn-large">EDITAR MI SUBASTA</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                        <div class="col-xs-10">
-                            <p><?= $SubastaDetalle->descripcion; ?></p>
+                        <div>
+                            <div class=" col-xs-10">
+                                <div class="project-description">
+                                    <div class="project-title clearfix">
+                                        <h3><strong><?= $SubastaDetalle->nombreProducto; ?></strong></h3>
+                                    </div>
+                                    <div class="project-info">
+                                        <div>
+                                            <span>Fecha</span><strong><?= $SubastaDetalle->fechaSubasta; ?></strong></div>
+                                        <div>
+                                            <span>Lote</span>
+                                            <?php if ($SubastaDetalle->IsLote == 1) { ?>
+                                                <strong>SI</strong> </div>
+                                        <?php } else { ?>
+                                            <strong>NO</strong> </div>
+                                    <?php } ?>
+                                    <div>
+                                        <span>Producto</span><strong><?= $SubastaDetalle->TipoProductoNombre; ?></strong></div>
+                                </div>
+                                <div class="col-xs-10">
+                                    <p><?= $SubastaDetalle->descripcion; ?></p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <!--FIN INSCRITO -->
+                    <!--FIN PROPIETARIO -->
+                <?php } else { ?>
+                    fdfd
+                    <?php
+                }
+            }
+            ?>
+        <?php } ?>
+
+    <?php } ?>
+<?php } else { ?>
+
+<?php } ?>
+
+
+<?php if ($Inscritos) { ?>
+    <?php $EXISTE = FALSE; ?>
+    <?php foreach ($Inscritos as $i) { ?>
+        <?php if ($this->session->userdata('id') == $i->UsuarioInscrito) { ?>
+            <?php $EXISTE = TRUE; ?>
         <?php } ?>
     <?php } ?>
-    <?php
-} else {
-    if ($this->session->userdata('id') == $SubastaDetalle->Propietario) {
-        ?>
-        <!--PROPIETARIO -->
+    <?php if ($EXISTE == FALSE & $this->session->userdata('id') != $SubastaDetalle->Propietario) { ?>
+        <!--SUSCRIBETE -->
         <div>
             <div class="pull-right col-xs-5">
                 <?php if ($SubastaDetalle->foto) { ?>
@@ -107,7 +184,7 @@
                 <form class="form-horizontal">
                     <div class="input-group col-xs-12">
                         <div class="input-group-addon">
-                            <button type="submit" class="btn btn-primary btn-large">EDITAR MI SUBASTA</button>
+                            <button type="submit" class="btn btn-primary btn-large">SUSCRIBIRSE</button>
                         </div>
                     </div>
                 </form>
@@ -131,6 +208,8 @@
                     <?php } ?>
                     <div>
                         <span>Producto</span><strong><?= $SubastaDetalle->TipoProductoNombre; ?></strong></div>
+                    <div>
+                        <span>Inscripcion</span><br><strong><?= $SubastaDetalle->fechaInscripcion; ?></strong></div>
                 </div>
                 <div class="col-xs-10">
                     <p><?= $SubastaDetalle->descripcion; ?></p>
@@ -138,75 +217,12 @@
             </div>
         </div>
         </div>
-        <!--FIN PROPIETARIO -->
-    <?php } else { ?>
-
-        <?php
-    }
-}
-?>
-                <?php } else { ?>
-                    <!--SUSCRIBETE -->
-                    <div>
-                        <div class="pull-right col-xs-5">
-                            <?php if ($SubastaDetalle->foto) { ?>
-                                <img class="img-thumbnail" src="<?= base_url(); ?>public/subasta_ci/producto/<?= $SubastaDetalle->foto; ?>" alt="project 3" >
-                            <?php } else { ?>
-                                <img class="img-thumbnail" src="<?= base_url(); ?>public/images/Portfolio01.png" alt="project 3" >
-                            <?php } ?>
-                        </div>
-                        <div class=" col-xs-5">
-                            <div class="project-title"></div>
-                            <div class="project-info">
-                                <h1><?= $SubastaDetalle->nombreProducto; ?></h1>
-                                <hr>
-                                <div class="row-margin">
-                                    <span>Monto Inicial</span> 
-                                    <span><h2><strong>$<?= $SubastaDetalle->precioSujerido; ?></strong></h2></span>
-                                </div>
-                            </div>
-                            <form class="form-horizontal">
-                                <div class="input-group col-xs-12">
-                                    <div class="input-group-addon">
-                                        <button type="submit" class="btn btn-primary btn-large">SUSCRIBIRSE</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <div>
-                        <div class=" col-xs-10">
-                            <div class="project-description">
-                                <div class="project-title clearfix">
-                                    <h3><strong><?= $SubastaDetalle->nombreProducto; ?></strong></h3>
-                                </div>
-                                <div class="project-info">
-                                    <div>
-                                        <span>Fecha</span><strong><?= $SubastaDetalle->fechaSubasta; ?></strong></div>
-                                    <div>
-                                        <span>Lote</span>
-                                        <?php if ($SubastaDetalle->IsLote == 1) { ?>
-                                            <strong>SI</strong> </div>
-                                    <?php } else { ?>
-                                        <strong>NO</strong> </div>
-                                <?php } ?>
-                                <div>
-                                    <span>Producto</span><strong><?= $SubastaDetalle->TipoProductoNombre; ?></strong></div>
-                                <div>
-                                    <span>Inscripcion</span><br><strong><?= $SubastaDetalle->fechaInscripcion; ?></strong></div>
-                            </div>
-                            <div class="col-xs-10">
-                                <p><?= $SubastaDetalle->descripcion; ?></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!--FIN SUSCRIBITE -->
-            <?php } ?>
-        <?php } ?>
+        <!--FIN SUSCRIBITE -->
     <?php } ?>
 
-    
+<?php } ?>
+
+
 </div>
 </div>
 <!-- Service section end -->
