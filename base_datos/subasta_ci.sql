1faaa7b0ee1ecad79fb6a72bb3dd523115e5591f
Dario Motto <dario_aratto@hotmail.com>
2016-12-01 20:23:36 -0600

    BackUp Script Base

--- base_datos/subasta_ci.sql
+++ base_datos/subasta_ci.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-11-2016 a las 09:07:50
+-- Tiempo de generación: 02-12-2016 a las 03:22:47
 -- Versión del servidor: 10.1.13-MariaDB
 -- Versión de PHP: 7.0.8
 
@@ -33,6 +33,14 @@ CREATE TABLE `catalogosubasta` (
   `Subasta_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `catalogosubasta`
+--
+
+INSERT INTO `catalogosubasta` (`id`, `IsActiva`, `Usuario_id`, `Subasta_id`) VALUES
+(1, 1, 2, 1),
+(2, 1, 3, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +54,15 @@ CREATE TABLE `inscripcion` (
   `CatalogoSubasta_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `inscripcion`
+--
+
+INSERT INTO `inscripcion` (`id`, `fechaInscripcion`, `Usuario_id`, `CatalogoSubasta_id`) VALUES
+(1, '2016-11-29', 3, 1),
+(2, '2016-11-29', 2, 2),
+(3, '2016-11-27', 4, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -56,8 +73,30 @@ CREATE TABLE `oferta` (
   `id` int(11) NOT NULL,
   `valor` float NOT NULL,
   `Usuario_id` int(11) NOT NULL,
-  `Subasta_id` int(11) NOT NULL
+  `Subasta_id` int(11) NOT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `oferta`
+--
+
+INSERT INTO `oferta` (`id`, `valor`, `Usuario_id`, `Subasta_id`, `updated_at`, `created_at`) VALUES
+(15, 255, 2, 2, '2016-12-02 04:59:50', '2016-12-02 04:59:50'),
+(16, 257, 2, 2, '2016-12-02 06:41:53', '2016-12-02 06:41:53'),
+(17, 258, 2, 2, '2016-12-02 06:42:28', '2016-12-02 06:42:28'),
+(18, 258, 2, 2, '2016-12-01 23:54:55', '2016-12-01 23:54:55'),
+(19, 260, 2, 2, '2016-12-01 23:54:59', '2016-12-01 23:54:59'),
+(20, 274, 2, 2, '2016-12-01 23:56:04', '2016-12-01 23:56:04'),
+(21, 276, 2, 2, '2016-12-01 23:56:10', '2016-12-01 23:56:10'),
+(22, 277, 4, 2, '2016-12-01 23:59:59', '2016-12-01 23:59:59'),
+(23, 281, 4, 2, '2016-12-02 00:00:09', '2016-12-02 00:00:09'),
+(24, 290, 2, 2, '2016-12-02 01:59:48', '2016-12-02 01:59:48'),
+(25, 300, 4, 2, '2016-12-02 02:00:01', '2016-12-02 02:00:01'),
+(26, 500, 2, 2, '2016-12-02 02:00:09', '2016-12-02 02:00:09'),
+(27, 1000, 4, 2, '2016-12-02 02:00:17', '2016-12-02 02:00:17'),
+(28, 1500, 2, 2, '2016-12-02 02:00:26', '2016-12-02 02:00:26');
 
 -- --------------------------------------------------------
 
@@ -69,9 +108,17 @@ CREATE TABLE `producto` (
   `id` int(11) NOT NULL,
   `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `IsLote` int(11) NOT NULL DEFAULT '0',
-  `foto` longblob,
+  `foto` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `TipoProducto_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id`, `nombre`, `IsLote`, `foto`, `TipoProducto_id`) VALUES
+(1, 'Camisa Polo', 1, 'camisa_polo.jpg', 2),
+(3, 'Cocina', 0, 'cocina.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -81,11 +128,23 @@ CREATE TABLE `producto` (
 
 CREATE TABLE `subasta` (
   `id` int(11) NOT NULL,
-  `fechaSubasta` date NOT NULL,
+  `fechaSubasta` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `precioSujerido` float NOT NULL,
-  `tiempoDuracionMin` int(11) NOT NULL,
-  `Producto_id` int(11) NOT NULL
+  `precioActual` float DEFAULT NULL,
+  `fechaFinal` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `descripcion` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Producto_id` int(11) NOT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `FINALIZADA` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `subasta`
+--
+
+INSERT INTO `subasta` (`id`, `fechaSubasta`, `precioSujerido`, `precioActual`, `fechaFinal`, `descripcion`, `Producto_id`, `updated_at`, `FINALIZADA`) VALUES
+(1, '2016-12-02 01:50:21', 50, 50, '0000-00-00 00:00:00', 'Esto si esta chivo compralo', 1, '2016-12-02 01:50:21', 1),
+(2, '2016-12-02 02:18:02', 250, 1500, '2016-12-02 02:18:00', 'Puja papu', 3, '2016-12-02 02:18:02', 1);
 
 -- --------------------------------------------------------
 
@@ -124,6 +183,14 @@ CREATE TABLE `tipoproducto` (
   `id` int(11) NOT NULL,
   `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `tipoproducto`
+--
+
+INSERT INTO `tipoproducto` (`id`, `nombre`) VALUES
+(1, 'Electronicos'),
+(2, 'Moda y Belleza');
 
 -- --------------------------------------------------------
 
@@ -167,7 +234,8 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`id`, `nombre`, `apellido`, `correo`, `password`, `apodo`, `capacidadPago`, `TipoUsuario_id`) VALUES
 (2, 'Dario', 'Motto', 'dario.motto@hotmail.com', '12345', 'ARATTO', 10000, 1),
-(3, 'Roman', 'Araya', 'dar@motto.c', '12345', 'MOTTO', 2000, 2);
+(3, 'Roman', 'Araya', 'dar@motto.c', '12345', 'MOTTO', 2000, 2),
+(4, 'Andre ', 'Leto', 'andre@leto.com', '12345', 'Leto', 10000, 2);
 
 --
 -- Índices para tablas volcadas
@@ -253,27 +321,27 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `catalogosubasta`
 --
 ALTER TABLE `catalogosubasta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `inscripcion`
 --
 ALTER TABLE `inscripcion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `oferta`
 --
 ALTER TABLE `oferta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `subasta`
 --
 ALTER TABLE `subasta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `subastaterminada`
 --
@@ -288,17 +356,17 @@ ALTER TABLE `tarifacomprador`
 -- AUTO_INCREMENT de la tabla `tipoproducto`
 --
 ALTER TABLE `tipoproducto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `tipousuario`
 --
 ALTER TABLE `tipousuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Restricciones para tablas volcadas
 --
